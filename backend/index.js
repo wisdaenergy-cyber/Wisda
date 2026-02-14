11468e40fb5838722bf78ddd3e5408055b20c6a7
wisdaenergy-cyber <wisdaenergy@gmail.com>
2026-02-14 17:03:19 +0530

    fix port for render deployment
    Refactor server listening logic to use environment variable for port.

--- backend/index.js
+++ backend/index.js
@@ -18,4 +18,8 @@ app.use("/api/v1", rootRouter);
 io.on("connection", (socket) => {
   console.log("Dashboard connected:", socket.id);
 });
-server.listen(3001);
+// server.listen(3001);
+const PORT = process.env.PORT || 3001;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
